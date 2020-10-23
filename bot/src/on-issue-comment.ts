a50645a093af2978c6abf65a1774fceed00ee684
Kevin Goslar <kevin.goslar@gmail.com>
2020-10-22 21:25:40 -0500

    progress

--- bot/src/on-issue-comment.ts
+++ bot/src/on-issue-comment.ts
@@ -1,6 +1,8 @@
 import * as probot from "probot"
 import webhooks from "@octokit/webhooks"
 
+const commandRE = /^\/([\w]+)\b *(.*)?$/
+
 export function onIssueComment(context: probot.Context<webhooks.WebhookPayloadIssueComment>): void {
   let org = ""
   let repo = ""
@@ -10,8 +12,31 @@ export function onIssueComment(context: probot.Context<webhooks.WebhookPayloadIs
     repo = context.payload.repository.name
     issue = context.payload.issue.number
     const repoPrefix = `${org}/${repo}`
-    const text = context.payload.comment.body
-    console.log(`${repoPrefix}: NEW COMMENT ON ISSUE #${issue}: ${text}`)
+    const text = context.payload.comment.body.trim()
+    console.log(`${repoPrefix}: NEW COMMENT ON ISSUE #${issue}:\n${text}`)
+    if (text === "") {
+      console.log(`${repoPrefix}: NO COMMAND GIVEN`)
+      // postReply("no command given", "Please provide a command")
+      return
+    }
+    if (!text.startsWith("/")) {
+      console.log(`${repoPrefix}: NOT A BOT COMMAND, IGNORING`)
+      return
+    }
+    const command = text.match(commandRE)
+    console.log(command)
+    if (command == null) {
+      throw new Error("unexpected NULL result")
+    }
+    if (command[0] !== "prettifier") {
+      console.log(`${repoPrefix}: NOT A PRETTIFIER COMMAND, IGNORING`)
+      return
+    }
+    if (command[1] === "debug") {
+      console.log(`${repoPrefix}: DEBUGGING`)
+      return
+    }
+    console.log(`${repoPrefix}: UNKNOWN COMMAND: ${command}`)
   } catch (e) {
     console.log(e)
   }
