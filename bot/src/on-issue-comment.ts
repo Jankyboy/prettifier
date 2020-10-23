6010023ce842a2eadb35e484cd4716f4dffddcef
Kevin Goslar <kevin.goslar@gmail.com>
2020-10-22 21:42:03 -0500

    progress

--- bot/src/on-issue-comment.ts
+++ bot/src/on-issue-comment.ts
@@ -7,21 +7,22 @@ const commandRE = /^\/([\w]+)\b *(.*)?$/
 export function onIssueComment(context: probot.Context<webhooks.WebhookPayloadIssueComment>): void {
   let org = ""
   let repo = ""
-  let issue = ""
+  let issueID = ""
+  let issueNr = 0
   try {
     org = context.payload.repository.owner.login
     repo = context.payload.repository.name
-    issue = context.payload.issue.node_id
+    issueID = context.payload.issue.node_id
+    issueNr = context.payload.issue.number
     const repoPrefix = `${org}/${repo}`
     const text = context.payload.comment.body.trim()
-    console.log(`${repoPrefix}: NEW COMMENT ON ISSUE #${issue}:\n${text}`)
+    console.log(`${repoPrefix}: NEW COMMENT ON ISSUE #${issueNr}:\n${text}`)
     if (text === "") {
       console.log(`${repoPrefix}: NO COMMAND GIVEN`)
-      addComment(issue, "no command given\n\nValid commands are: debug", context.github)
+      addComment(issueID, "no command given\n\nValid commands are: debug", context.github)
       return
     }
     const command = text.match(commandRE)
-    console.log(command)
     if (command == null) {
       console.log(`${repoPrefix}: NOT A BOT COMMAND, IGNORING`)
       return
