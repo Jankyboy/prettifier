333593ff6ffbd0121fc36bdd04292f039743219b
Kevin Goslar <kevin.goslar@gmail.com>
2020-10-22 20:39:52 -0500

    permissions

--- bot/src/on-issue-comment.ts
+++ bot/src/on-issue-comment.ts
@@ -2,6 +2,17 @@ import * as probot from "probot"
 import webhooks from "@octokit/webhooks"
 
 export function onIssueComment(context: probot.Context<webhooks.WebhookPayloadIssueComment>): void {
-  const text = context.payload.comment.body
-  console.log(text)
+  let org = ""
+  let repo = ""
+  let issue = 0
+  try {
+    org = context.payload.repository.owner.login
+    repo = context.payload.repository.name
+    issue = context.payload.issue.id
+    const repoPrefix = `${org}/${repo}`
+    const text = context.payload.comment.body
+    console.log(`${repoPrefix}: NEW COMMENT ON ISSUE #${issue}: ${text}`)
+  } catch (e) {
+    console.log(e)
+  }
 }
